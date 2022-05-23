246c94cffbe90d037dbc2d04a24b879004bcd2b9
Devinoraya Radityapalma <devinoraya.radityapalma@gmail.com>
2022-05-23 20:09:49 +0700

    write Lottery component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
-// import Lottery from './Lottery';
-import LottoBall from './LottoBall';
+// import LottoBall from './LottoBall';
+import Lottery from './Lottery';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      {/* <Lottery /> */}
-      <LottoBall num={25} />
-      <LottoBall num={20} />
-      <LottoBall num={2} />
-      <LottoBall num={15} />
+      <Lottery />
+      <Lottery title="Lotto Mini" numBalls={4} maxNum={10} />
     </div>
   );
 }
